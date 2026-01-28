1178ad2b843a37ecd00823f534f08bae879400cd
pedrojuanzzinfor <pedrojuanzzinfor2k25@gmail.com>
2026-01-28 19:27:19 -0300

    implementei a mudança de tema

--- sript.js
+++ sript.js
@@ -1,11 +1,20 @@
 import { criarEstado } from "./minireact.js";
 
 const [lerTarefas, alterarTarefas] = criarEstado(['aprender JS', 'Entender REACT'], render)
+const [lerTema, alterarTema] = criarEstado('claro', render)
+const botaoTema = document.querySelector("#tema")
 const btn_novaTarefa = document.querySelector("#btn-add")
 btn_novaTarefa.addEventListener("click", function () {
     const tarefas = (lerTarefas())
     tarefas.push((document.querySelector("#input-task").value))
     alterarTarefas(tarefas)
+})
+botaoTema.addEventListener("click", function () {
+
+    if (lerTema() == "claro") {
+        alterarTema("escuro")
+    }
+    else alterarTema("claro")
 })
 
 render()
@@ -18,7 +27,7 @@ function App() {
     }).join(" ")) + "</ul>"
 }
 function render() {
-    console.log(App())
+    document.querySelector("body").className = lerTema()
     const div = document.querySelector("#app")
     div.innerHTML = App()
 }
